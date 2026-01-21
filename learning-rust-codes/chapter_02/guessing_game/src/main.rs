ce854bef9b61905d2b18a7fd0a770bfaaab059d2
leocjj <leocj@hotmail.com>
2026-01-20 23:25:47 -0500

    Refactor main function in guessing game for improved structure and clarity

--- learning-rust-codes/chapter_02/guessing_game/src/main.rs
+++ learning-rust-codes/chapter_02/guessing_game/src/main.rs
@@ -1,15 +1,17 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-
+    // FIRST EXAMPLE
+    println!("First part of the example:");
+    let x = 7;
+    let y = 10;
     // An unsued variable example with _, to avoid warning.
     let _apples = 5; // immutable
-    let x = 5;
-    let y = 10;
     println!("x = {x} and y + 2 = {}", y + 2);
 
+    println!("Guess the number!");
+    println!("Please input your guess.");
     let mut guess = String::new();
 
     let r1 = io::stdin().read_line(&mut guess);
@@ -25,4 +27,29 @@ fn main() {
 
     guess.clear(); // Clear the previous input
     println!("Clear guess: '{guess}'");
+
+    // Generate a random number between 1 and 100
+    println!("Second part of the example:");
+
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {guess}");
+
+    // SECOND EXAMPLE
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {guess}");
 }
