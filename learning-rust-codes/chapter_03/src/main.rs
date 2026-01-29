a452c01466c7d014a070e2a2fcd069aa222fe232
leocjj <leocj@hotmail.com>
2026-01-29 01:31:56 -0500

    Add chapter 03 with examples of variables, mutability, and shadowing in Rust

---
+++ learning-rust-codes/chapter_03/src/main.rs
@@ -0,0 +1,23 @@
+const GLOBAL_VARIABLE: i32 = 10; // Global variable
+
+fn main() {
+    // Variables and Mutability
+    // let x = 5; // Immutable variable, cannot be changed
+    let mut x = 5; // Mutable variable, can be changed
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // Shadowing
+    let x = 5; // First variable is shadowed by the second
+    let x = x + 1; // Overshadows the first and is shadowed by the third inside the next block.
+    {
+        // inner scope
+        let x = x * 2; // This creates a new variable x that shadows the previous one
+        println!("The value of x in the inner scope is: {x}");
+    }
+    println!("The value of x is: {x}");
+}
